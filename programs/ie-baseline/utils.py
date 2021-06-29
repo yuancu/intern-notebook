f06655b733ba9ae9fb8f55b7d6f914fbbe3c6a50
Yuanchun <yuanchu@amazon.com>
2021-06-29 12:01:18 +0800

    debugged para_eval

--- programs/ie-baseline/utils.py
+++ programs/ie-baseline/utils.py
@@ -23,7 +23,7 @@ def extract_spoes(texts, tokens, subject_model, object_model, id2predicate):
     # magic numbers come from https://github.com/bojone/bert4keras/blob/master/examples/task_relation_extraction.py
     batch_size = subject_preds.shape[0]
     spoes = []
-    for k in batch_size:
+    for k in range(batch_size):
         sub_start = np.where(subject_preds[k, :, 0] > 0.6)[0]
         sub_end = np.where(subject_preds[k, :, 1] > 0.5)[0]
         subjects = []
@@ -33,10 +33,12 @@ def extract_spoes(texts, tokens, subject_model, object_model, id2predicate):
                 j = j[0]
                 subjects.append((i, j))
         if subjects:
-            subjects = np.array(subjects)
-            # create pseudo batch
-            pseudo_states = hidden_states[k].repeat(len(subjects))
-            object_preds = object_model(pseudo_states, subjects[:, 0], subjects[:, 1])
+            subjects = torch.tensor(subjects)
+            # create pseudo batch: repeat k-th embedding on newly inserted dim 0
+            pseudo_states = torch.stack([hidden_states[k]]*len(subjects), dim=0) # (len(subjects), sent_len, emb_size)
+            extracted_sub_start = subjects[:, 0].view(-1, 1)
+            extracted_sub_end = subjects[:, 1].view(-1, 1)
+            object_preds = object_model(pseudo_states, extracted_sub_start, extracted_sub_end)
             for subject, object_pred in zip(subjects, object_preds):
                 obj_start = np.where(object_pred[:, :, 0] > 0.6)
                 obj_end = np.where(object_pred[:, :, 1] > 0.5)
@@ -59,7 +61,9 @@ def para_eval(subject_model, object_model, loader, id2predicate, batch_eval=Fals
         texts, tokens, spoes, att_masks = batch
         print("hello")
         R = set(extract_spoes(texts, tokens, subject_model, object_model, id2predicate))
-        T = set([tuple(spo) for spo in spo_list  for spo_list in spoes])
+        T = set()
+        for spo_list in spoes:
+            T.update([tuple(spo) for spo in spo_list])
         A += len(R & T)
         B += len(R)
         C += len(T)
@@ -71,16 +75,16 @@ def para_eval(subject_model, object_model, loader, id2predicate, batch_eval=Fals
 
 
 
-
 def extract_items(text_in, tokenizer, s_m, po_m, id2predicate):
     R = []
     output = tokenizer.encode_plus(text_in, max_length=config.max_sentence_len, truncation=True, 
                     pad_to_max_length=True, return_tensors="pt")
-    hidden_states = output['input_ids']
+    tokens = output['input_ids']
     attention_mask = output['attention_mask']
     attention_mask = attention_mask.unsqueeze(dim=2)
-    _k1, _k2, t, t_max= s_m(hidden_states.to(device), attention_mask.to(device))
-    _k1, _k2 = _k1[0, :, 0], _k2[0, :, 0]
+    subject_preds, hidden_states = s_m(tokens.to(device))
+    _k1, _k2 = subject_preds[0, :, 0], subject_preds[0, :, 1] # _k1: (1, sent_len)
+    t = hidden_states
     _kk1s = []
     for i, _kk1 in enumerate(_k1):
         if _kk1 > 0.5:
@@ -92,8 +96,8 @@ def extract_items(text_in, tokenizer, s_m, po_m, id2predicate):
             if _subject:
                 _k1, _k2 = torch.LongTensor([[i]]), torch.LongTensor(
                     [[i+j]])  # np.array([i]), np.array([i+j])
-                _o1, _o2 = po_m(t.to(device), t_max.to(
-                    device), _k1.to(device), _k2.to(device))
+                object_preds = po_m(hidden_states, _k1, _k2)
+                _o1, _o2 = object_preds[:,:, 0], object_preds[:,:, 1]
                 _o1, _o2 = _o1.cpu().data.numpy(), _o2.cpu().data.numpy()
 
                 _o1, _o2 = np.argmax(_o1[0], 1), np.argmax(_o2[0], 1)
